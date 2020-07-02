e3c8e973529b81b6157f877d5b03beea848c8e52
sixer51 <yijia.wu.gr@dartmouth.edu>
2020-07-02 01:08:20 -0400

    Add MyAlgorithm

--- test_gui_src/rqt_kinematics/src/rqt_kinematics/interfaces/MyAlgorithm.py
+++ test_gui_src/rqt_kinematics/src/rqt_kinematics/interfaces/MyAlgorithm.py
@@ -1,4 +1,5 @@
 import threading
+import numpy
 
 
 class Algorithm:
@@ -9,5 +10,88 @@ class Algorithm:
         self.pick_place = pick_place
 
     def myalgorithm(self, event):
+        self.pick_place.back_to_home()
+
+        # insert following two lines where you want to stop the algorithm 
+        # with the stop button in GUI
+        if not event.isSet():
+            return
+
+        # pick cylinder
+        object_name = "cylinder"
+        pose = self.pick_place.get_object_pose(object_name)
+        print(pose.position.y)
+        pose.position.z += 0.16
+        self.pick_place.pickup(object_name, pose)
+
+        if not event.isSet():
+            return
+
+        roll = 0.0
+        pitch = numpy.deg2rad(90.0)
+        yaw = 0.0
+        x = 0
+        y = 0.6
+        z = pose.position.z + 0.01
+        place_pose = self.pick_place.pose2msg(roll, pitch, yaw, x, y, z)
+        self.pick_place.place(place_pose)
+
+        if not event.isSet():
+            return
+
+        self.pick_place.back_to_home()
+
+        if not event.isSet():
+            return
+
+        # pick box
+        object_name = "box"
+        pose = self.pick_place.get_object_pose(object_name)
+        print(pose.position.y)
+        pose.position.z += 0.15
+        self.pick_place.pickup(object_name, pose)
+
+        if not event.isSet():
+            return
+
+        roll = 0.0
+        pitch = numpy.deg2rad(90.0)
+        yaw = 0.0
+        x = 0.15
+        y = 0.6
+        z = pose.position.z + 0.01
+        place_pose = self.pick_place.pose2msg(roll, pitch, yaw, x, y, z)
+        self.pick_place.place(place_pose)
+
+        if not event.isSet():
+            return
+
+        self.pick_place.back_to_home()
+
+        if not event.isSet():
+            return
+
+        # pick ball
+        object_name = "ball"
+        pose = self.pick_place.get_object_pose(object_name)
+        print(pose.position.y)
+        pose.position.z += 0.14
+        self.pick_place.pickup(object_name, pose)
+
+        if not event.isSet():
+            return
+
+        roll = 0.0
+        pitch = numpy.deg2rad(90.0)
+        yaw = 0.0
+        x = -0.15
+        y = 0.6
+        z = pose.position.z + 0.01
+        place_pose = self.pick_place.pose2msg(roll, pitch, yaw, x, y, z)
+        self.pick_place.place(place_pose)
+
+        if not event.isSet():
+            return
+
         self.pick_place.back_to_home()
 
